f9e0edea6ccf5f2921bd52c8361e272aa2f0c893
sampath bodakunti <sampathbodakunti@gmail.com>
2025-08-17 20:53:13 +0530

    Add files via upload

---
+++ Demostudent.java
@@ -0,0 +1,29 @@
+//ENCAPSULATION
+public class Demostudent {
+	private int sid;
+    private String sname;
+    public int getSid() {
+		return sid;
+	}
+    public void setSid(int sid) {
+		this.sid = sid;
+	}
+    public String getSname() {
+		return sname;
+	}
+    public void setSname(String sname) {
+		this.sname = sname;
+	}
+    public void display() {
+    	System.out.println("The student details are:"+sid +sname);
+    }
+    public static void main(String[] args) {
+    
+	Demostudent obj=new Demostudent();
+	obj.setSid(101);
+	obj.setSname("sampath");
+	System.out.println(obj.getSid());
+	System.out.println(obj.getSname());
+}
+
+}
